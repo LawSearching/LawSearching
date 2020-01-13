26aecef7864f27e9d9949959c5e1413b165b6efb
Sylar47 <septemberman@outlook.com>
2020-01-13 11:49:18 +0800

    drawer内容更新

--- lawsearching/lib/main_page.dart
+++ lawsearching/lib/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -6,8 +7,11 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  GlobalKey<FormState> searchKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context, width: 2048, height: 2732, allowFontScaling: true);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'MainPage',
@@ -15,10 +19,188 @@ class _MainPageState extends State<MainPage> {
       home: Scaffold(
         drawer: Drawer(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              
+              Container(
+                alignment: Alignment.center,
+                height: ScreenUtil().setHeight(456),
+                child: Text(
+                  '中国法律法规大全',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: ScreenUtil().setSp(70),
+                  ),
+                ),
+              ),
+              _lawList(),
             ],
           ),
+        ),
+        body: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              '法律法规检索',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _lawList() {
+    return Container(
+      height: ScreenUtil().setHeight(2276),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                '宪法',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '刑法',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '民法',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '诉讼',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '行政',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '商法',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '劳动法',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '经济法',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '国际法',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '常用法律',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '最新法律',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '全部法律',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '司法解释',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text(
+                '房地产',
+                style: TextStyle(
+                  fontSize: ScreenUtil().setSp(58),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              onTap: () {},
+            ),
+          ],
         ),
       ),
     );
