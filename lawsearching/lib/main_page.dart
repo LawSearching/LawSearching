eca76a874a3631137d46621e2316acd926f3657c
Sylar47 <septemberman@outlook.com>
2020-01-13 17:20:09 +0800

    功能更新
    1、drawerUI美化；
    2、主界面内容添加。

--- lawsearching/lib/main_page.dart
+++ lawsearching/lib/main_page.dart
@@ -8,6 +8,13 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   GlobalKey<FormState> searchKey = GlobalKey<FormState>();
+  String searchByName = '';
+  void search() {
+    var searchForm = searchKey.currentState;
+    if (searchForm.validate()) {
+      searchForm.save();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +30,16 @@ class _MainPageState extends State<MainPage> {
             children: <Widget>[
               Container(
                 alignment: Alignment.center,
-                height: ScreenUtil().setHeight(456),
+                height: ScreenUtil().setHeight(550),
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue[800],
+                ),
                 child: Text(
                   '中国法律法规大全',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: ScreenUtil().setSp(70),
+                    color: Colors.white,
                   ),
                 ),
               ),
@@ -42,12 +53,74 @@ class _MainPageState extends State<MainPage> {
               '法律法规检索',
               style: TextStyle(
                 color: Colors.white,
+                fontSize: ScreenUtil().setSp(55),
               ),
             ),
             centerTitle: true,
           ),
-          body: Column(
-            children: <Widget>[],
+          body: Container(
+            child: Column(
+              children: <Widget>[
+                Form(
+                  key: searchKey,
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+                    width: ScreenUtil().setWidth(2048),
+                    height: ScreenUtil().setHeight(400),
+                    // decoration: BoxDecoration(
+                    //   border: Border(
+                    //     bottom: BorderSide(
+                    //       color: Colors.blueGrey,
+                    //       width: 1.0,
+                    //       style: BorderStyle.solid
+                    //     )
+                    //   )
+                    // ),
+                    child: TextFormField(
+                      autofocus: false,
+                      style: TextStyle(
+                          color: Colors.black38,
+                          fontSize: ScreenUtil().setSp(55)),
+                      decoration: InputDecoration(hintText: '搜索'),
+                      textAlign: TextAlign.center,
+                      obscureText: false,
+                      onSaved: (value) {
+                        searchByName = value;
+                      },
+                      validator: (value) {
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: ScreenUtil().setWidth(2048),
+                  height: ScreenUtil().setHeight(1655),
+                  child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return InkWell(
+                        onTap: () {},
+                        child: Container(
+                          margin: EdgeInsets.all(5.0),
+                          padding: EdgeInsets.only(left: 30, top: 20),
+                          decoration: BoxDecoration(
+                              // color: Colors.white,
+                              border: Border(
+                                  bottom: BorderSide(
+                            style: BorderStyle.solid,
+                            color: Colors.grey,
+                            width: 1.0,
+                          ))),
+                          child: Text('data'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -56,7 +129,18 @@ class _MainPageState extends State<MainPage> {
 
   Widget _lawList() {
     return Container(
-      height: ScreenUtil().setHeight(2276),
+      height: ScreenUtil().setHeight(2182),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: <Color>[
+            Colors.lightBlue[800],
+            // Colors.blueAccent[100],
+            Colors.lightBlueAccent,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomRight,
+        ),
+      ),
       child: SingleChildScrollView(
         child: Column(
           children: <Widget>[
@@ -65,6 +149,7 @@ class _MainPageState extends State<MainPage> {
                 '宪法',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -75,6 +160,7 @@ class _MainPageState extends State<MainPage> {
                 '刑法',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -85,6 +171,7 @@ class _MainPageState extends State<MainPage> {
                 '民法',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -95,6 +182,7 @@ class _MainPageState extends State<MainPage> {
                 '诉讼',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -105,6 +193,7 @@ class _MainPageState extends State<MainPage> {
                 '行政',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -115,6 +204,7 @@ class _MainPageState extends State<MainPage> {
                 '商法',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -125,6 +215,7 @@ class _MainPageState extends State<MainPage> {
                 '劳动法',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -135,6 +226,7 @@ class _MainPageState extends State<MainPage> {
                 '经济法',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -145,6 +237,7 @@ class _MainPageState extends State<MainPage> {
                 '国际法',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -155,6 +248,7 @@ class _MainPageState extends State<MainPage> {
                 '常用法律',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -165,6 +259,7 @@ class _MainPageState extends State<MainPage> {
                 '最新法律',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -175,6 +270,7 @@ class _MainPageState extends State<MainPage> {
                 '全部法律',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -185,6 +281,7 @@ class _MainPageState extends State<MainPage> {
                 '司法解释',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -195,6 +292,7 @@ class _MainPageState extends State<MainPage> {
                 '房地产',
                 style: TextStyle(
                   fontSize: ScreenUtil().setSp(58),
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
