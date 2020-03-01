7c5c4cf2b7d42018555b83c56ccd375b2c041bd3
Kreminlin <287111077@qq.com>
2020-03-01 17:00:29 +0800

    完成了民用航空空中交通管理规则目录
    1）目录的创建
    2）按章、节显示具体内容

--- lawsearching/lib/pages/MingyongHangkong_rules/directory.dart
+++ lawsearching/lib/pages/MingyongHangkong_rules/directory.dart
@@ -1,8 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lawsearching/pages/MingyongHangkong_rules/fileReader.dart';
 import 'package:lawsearching/sqlManger/my_db_method.dart'; //导出sqflite通用方法
 import 'CivilAvationData.dart';
+import 'package:lawsearching/main_page.dart';
+
+var url_1 = "http://39.97.103.161:8080/queryChapter";
+List receive_data = [];
+List tempdirectorylist = [];
 
 class Civil_Aviation_Directory extends StatefulWidget {
   Civil_Aviation_Directory({Key key}) : super(key: key);
@@ -13,49 +19,8 @@ class Civil_Aviation_Directory extends StatefulWidget {
 }
 
 class _Civil_Aviation_DirectoryState extends State<Civil_Aviation_Directory> {
-  MyDbMethod myGeneralRulesMethod = new MyDbMethod();
+  List<Widget> secondaryCategory = [];
   List civilAvtionDirectorylistName = civilAvtionDirectorylist; //导入民用航空交通管理目录数据
-  String mingYongHangKong = "MingYongHangKong"; //表名
-  String cadDirectoryName = "CADirectoryName"; //列名
-  int num = 0; //统计获取的民用航空数据数量;
-  List getlist = []; //统计获取的民用航空数据;
-
-  /**
-   * 将民用航空交通管理目录数据存入my_db.db数据库中，表名为mingYongHangKong,列名为CADirectoryName
-   */
-
-  //存取民用航空章目录
-  _saveMingYongHangKong(String name) {
-    myGeneralRulesMethod.saveString(mingYongHangKong, cadDirectoryName, name);
-  }
-
-  //获取民用航空章目录数量
-  Future _getnumOfMingYongHangKong() async {
-    return await myGeneralRulesMethod.getString(
-        mingYongHangKong, cadDirectoryName); //返回值为int类型，显示数据库中的数据量;
-  }
-
-  //获取民用航空章目录数据
-  Future<List<Map>> _getMingYongHangKong() async {
-    return await myGeneralRulesMethod.searchString(
-        mingYongHangKong, cadDirectoryName); //返回值为List类型，显示数据库中数据;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _getnumOfMingYongHangKong().then((value) {
-      print(value);
-      //如果从数据库中未获取到数据，则向数据库中存入数据；
-      if (value == 0) {
-        print('当前民用航空表中无任何章节数据，现加载内容如下：');
-        for (String itemName in civilAvtionDirectorylistName) {
-          _saveMingYongHangKong(itemName); //一个一个存取章名
-          print('加载内容:$itemName');
-        }
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,63 +28,105 @@ class _Civil_Aviation_DirectoryState extends State<Civil_Aviation_Directory> {
       appBar: AppBar(
         title: Text('民用航空空中交通管理规则'),
       ),
-      body: FutureBuilder(
-        future: _getMingYongHangKong(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: 20,
-              itemBuilder: (context, index) {
-                return ExpansionTile(
-                  title: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => FileReader(
-                                    snapshot.data[index]['CADirectoryName']
-                                        .toString(),
-                                  )));
-                    },
-                    child: Text(
-                      snapshot.data[index]['CADirectoryName'].toString(),
-                      style: TextStyle(
-                          color: Colors.black45,
-                          fontSize: ScreenUtil().setSp(45.0)),
-                    ),
-                  ),
-                );
-              },
-            );
+      body: ListView.builder(
+        itemCount: civilAvtionDirectorylistName.length,
+        itemBuilder: (context, index1) {
+          secondaryCategory = [];
+          //判断目录为空
+          if (index1 == 0 ||
+              index1 == 7 ||
+              index1 == 8 ||
+              index1 == 9 ||
+              index1 == 18) {
+            tempdirectorylist = [];
           }
+          //判断第二章，并显示子目录
+          if (index1 == 1) {
+            tempdirectorylist = civilAvtionlist_Two;
+          }
+          //判断第三章，并显示子目录
+          if (index1 == 2) {
+            tempdirectorylist = civilAvtionlist_Three;
+          }
+          //判断第四章，并显示子目录
+          if (index1 == 3) {
+            tempdirectorylist = civilAvtionlist_Four;
+          }
+          //判断第五章，并显示子目录
+          if (index1 == 4) {
+            tempdirectorylist = civilAvtionlist_Five;
+          }
+          //判断第六章，并显示子目录
+          if (index1 == 5) {
+            tempdirectorylist = civilAvtionlist_Six;
+          }
+          //判断第七章，并显示子目录
+          if (index1 == 6) {
+            tempdirectorylist = civilAvtionlist_Seven;
+          }
+          //判断第十一章，并显示子目录
+          if (index1 == 10) {
+            tempdirectorylist = civilAvtionlist_Eleven;
+          }
+          //判断第十二章，并显示子目录
+          if (index1 == 11) {
+            tempdirectorylist = civilAvtionlist_Twelve;
+          }
+          //判断第十三章，并显示子目录
+          if (index1 == 12) {
+            tempdirectorylist = civilAvtionlist_Thirteen;
+          }
+          //判断第十四章，并显示子目录
+          if (index1 == 13) {
+            tempdirectorylist = civilAvtionlist_Fourteen;
+          }
+          //判断第十五章，并显示子目录
+          if (index1 == 14) {
+            tempdirectorylist = civilAvtionlist_Fifteen;
+          }
+          //判断第十六章，并显示子目录
+          if (index1 == 15) {
+            tempdirectorylist = civilAvtionlist_Sixteen;
+          }
+          //判断第十七章，并显示子目录
+          if (index1 == 16) {
+            tempdirectorylist = civilAvtionlist_Seventeen;
+          }
+          //判断第十八章，并显示子目录
+          if (index1 == 17) {
+            tempdirectorylist = civilAvtionlist_Eighteen;
+          }
+          //判断第二十章，并显示子目录
+          if (index1 == 19) {
+            tempdirectorylist = civilAvtionlist_Twenty;
+          }
+          for (var item in tempdirectorylist) {
+            secondaryCategory.add(new ListTile(
+              title: Text(item),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => FileReader(
+                            '${civilAvtionDirectorylistName[index1]}', item)));
+              },
+            ));
+          }
+          return ExpansionTile(
+            title: InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => FileReader(
+                            '${civilAvtionDirectorylistName[index1]}',
+                            'temp')));
+              },
+              child: Text(civilAvtionDirectorylistName[index1]),
+            ),
+            children: secondaryCategory,
+          );
         },
-      ),
-    );
-  }
-
-  Widget _topsearch() {
-    return Container(
-      padding: EdgeInsets.all(2.0),
-      alignment: Alignment.center,
-      height: ScreenUtil().setHeight(250),
-      width: ScreenUtil().setWidth(2048),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-      ),
-      margin: EdgeInsets.only(left: 10.0),
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
-            iconSize: 22.0,
-          ),
-          Text(
-            '',
-            style: TextStyle(fontSize: 13.0),
-          ),
-        ],
       ),
     );
   }
